94536073def691597754ee7b0645ef592a5b45a4
Vex87 <AndrewKyleGuilas@gmail.com>
2021-03-04 16:29:05 -0500

    more ACAS commands

--- cogs/class_alert.py
+++ cogs/class_alert.py
@@ -1,3 +1,11 @@
+BLACKLISTED_DAYS = [4, 5, 6]
+AUDIT_CHANNEL_ID = 813453150886428742
+REMINDER_BLOCK_TIMES = ["09:20:00", "10:50:00", "12:35:00", "14:35:00"]
+BLOCK_TIMES = ["09:25:00", "10:55:00", "12:40:00", "14:40:00"]
+GUILD_ID = 651133204492845066
+ROLE_ID = 816706859015995432
+CHANNEL_ID = 816702193675403324
+
 import discord
 from discord import Color as discord_color
 from discord.ext import commands, tasks
@@ -28,6 +36,7 @@ def create_embed(title, color = discord_color.blue(), fields = {}):
 class class_alert(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.is_paused = False
         self.class_alert.start()
 
     def cog_unload(self):
@@ -40,21 +49,15 @@ class class_alert(commands.Cog):
     async def class_alert(self):
         await self.client.wait_until_ready()
 
-        AUDIT_CHANNEL_ID = 813453150886428742
-        logs_channel = self.client.get_channel(AUDIT_CHANNEL_ID)
+        now = datetime.now(tz = pytz.timezone("US/Eastern"))
+        
+        if (now.datetime.today().weekday() in BLACKLISTED_DAYS) or (self.is_paused):
+            return
 
         try:
-            REMINDER_BLOCK_TIMES = ["09:20:00", "10:50:00", "12:35:00", "14:35:00"]
-            BLOCK_TIMES = ["09:25:00", "10:55:00", "12:40:00", "14:40:00"]
-            GUILD_ID = 651133204492845066
-            ROLE_ID = 816706859015995432
-            CHANNEL_ID = 816702193675403324
-
             guild = await self.client.fetch_guild(GUILD_ID)
             role = guild.get_role(ROLE_ID)
             channel = self.client.get_channel(CHANNEL_ID)
-
-            now = datetime.now(tz = pytz.timezone("US/Eastern"))
             current_time = str(now.strftime("%H:%M:%S"))
 
             if current_time in BLOCK_TIMES:
@@ -62,14 +65,13 @@ class class_alert(commands.Cog):
             elif current_time in REMINDER_BLOCK_TIMES:
                 await channel.send(f"{role.mention} Block {int(BLOCK_TIMES.index(current_time)) + 1} is starting in 5 minutes")
         except Exception as error_message:
+            logs_channel = self.client.get_channel(AUDIT_CHANNEL_ID)
             await logs_channel.send(embed = create_embed("ERROR: Something went wrong when running a class alert", discord_color.red(), {
                 "Error Message": error_message
             }))
 
     @commands.command()
     async def acas(self, context):
-        ROLE_ID = 816706859015995432
-
         member = context.author
         role = discord.utils.get(context.guild.roles, id = ROLE_ID)
         will_give_role = True
@@ -81,7 +83,17 @@ class class_alert(commands.Cog):
             await member.add_roles(role)
 
         fill_text = will_give_role and "now" or "will no longer"
-        await context.send(embed = create_embed(f"{member}, you will {fill_text} be notified 5 mintes and on the second class starts", discord_color.green()))
+        await context.send(embed = create_embed(f"{member}, you will {fill_text} be notified 5 mintes and on the minute class starts", discord_color.green()))
+
+    @commands.command()
+    async def pauseacas(self, context):
+        self.is_paused = False
+        await context.send(embed = create_embed("SUCCESS: ACAS is now paused and will not alarm since there is no school today", discord_color.green()))
+
+    @commands.command()
+    async def resumeacas(self, context):
+        self.is_paused = True
+        await context.send(embed = create_embed("SUCCESS: ACAS has resumed and will alarm as there is school today", discord_color.green()))          
 
 def setup(client):
     client.add_cog(class_alert(client))
