888e8266eda6766884247883d94bc5f0251fdc26
Vex87 <AndrewKyleGuilas@gmail.com>
2021-03-09 17:43:30 -0500

    fixed loading module in leveling system

--- cogs/leveling_system.py
+++ cogs/leveling_system.py
@@ -33,7 +33,7 @@ import time
 from datetime import datetime
 from pymongo import MongoClient
 
-from cogs.economy_system import give_money
+import cogs.economy_system as economy_system
 
 cluster = MongoClient("mongodb+srv://admin:QZnOT86qe3TQ@cluster0.meksl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 leveling = cluster.discord.leveling
@@ -99,7 +99,7 @@ def give_experience(user_id, amount):
             experience -= get_total_experience_of_level(level)
             level += 1
             new_level = level
-            give_money(user_id, "bank", get_money_for_leveling(level))
+            economy_system.give_money(user_id, "bank", get_money_for_leveling(level))
         else:
             break
 
