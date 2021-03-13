adcc50f847e86323fa393aa5bafe938e0ede2132
Vex87 <AndrewKyleGuilas@gmail.com>
2021-03-12 22:11:08 -0500

    updated main.py to support new embed function

--- __main__.py
+++ __main__.py
@@ -6,13 +6,13 @@ from discord import Color as discord_color
 
 import os
 import pytz
-import threading
 from datetime import datetime
 
-def create_embed(title, color = discord_color.blue(), fields = {}):
+def create_embed(title, fields: {} = {}, info: {} = {}):
     embed = discord.Embed(
         title = title,
-        colour = color or discord_color.blue()
+        colour = info.get("color") or discord_color.blue(),
+        timestamp = datetime.now(tz = pytz.timezone("US/Eastern"))
     )
 
     for name, value in fields.items():
@@ -22,12 +22,12 @@ def create_embed(title, color = discord_color.blue(), fields = {}):
             inline = True
         )
 
-    embed.timestamp = datetime.now(tz = pytz.timezone("US/Eastern"))
+    if info.get("member"):
+        embed.set_author(name = info["member"], icon_url = info["member"].avatar_url)
 
     return embed
 
-intents = discord.Intents.all()
-client = commands.Bot(command_prefix = "?", intents = intents)
+client = commands.Bot(command_prefix = "?", intents = discord.Intents.all())
 
 @client.command()
 @commands.check_any(commands.is_owner(), commands.has_permissions(administrator = True))
@@ -35,11 +35,17 @@ async def load(context, extention):
     try:
         client.load_extension(f"cogs.{extention}")
     except Exception as error_message:
-        await context.send(embed = create_embed(f"Error: {extention} was unable to load", discord_color.red(), {
+        await context.send(embed = create_embed(f"ERROR: {extention} was unable to load", {
             "Error Message": str(error_message)
+        }, {
+            "color": discord_color.red(),
+            "member":  context.author,
         }))
     else:
-        await context.send(embed = create_embed(f"Success: {extention} was loaded", discord_color.green()))
+        await context.send(embed = create_embed(f"SUCCESS: {extention} was loaded", {}, {
+            "color": discord_color.green(),
+            "member":  context.author,
+        }))
 
 @client.command()
 @commands.check_any(commands.is_owner(), commands.has_permissions(administrator = True))
@@ -47,11 +53,17 @@ async def unload(context, extention):
     try:
         client.unload_extension(f"cogs.{extention}")
     except Exception as error_message:
-        await context.send(embed = create_embed(f"Error: {extention} was unable to unload", discord_color.red(), {
+        await context.send(embed = create_embed(f"ERROR: {extention} was unable to unload", {
             "Error Message": str(error_message)
+        }, {
+            "color": discord_color.red(),
+            "member":  context.author,
         }))
     else:
-        await context.send(embed = create_embed(f"Success: {extention} was unloaded", discord_color.green()))
+        await context.send(embed = create_embed(f"SUCCESS: {extention} was unloaded", {}, {
+            "color": discord_color.green(),
+            "member":  context.author,
+        }))
 
 @client.command()
 @commands.check_any(commands.is_owner(), commands.has_permissions(administrator = True))
@@ -59,11 +71,17 @@ async def reload(context, extention):
     try:
         client.reload_extension(f"cogs.{extention}")
     except Exception as error_message:
-        await context.send(embed = create_embed(f"Error: {extention} was unable to reload", discord_color.red(), {
+        await context.send(embed = create_embed(f"ERROR: {extention} was unable to reload", {
             "Error Message": str(error_message)
+        }, {
+            "color": discord_color.red(),
+            "member":  context.author,
         }))
     else:
-        await context.send(embed = create_embed(f"Success: {extention} was reloaded", discord_color.green()))
+        await context.send(embed = create_embed(f"SUCCESS: {extention} was reloaded", {}, {
+            "color": discord_color.green(),
+            "member":  context.author,
+        }))
 
 @client.command()
 @commands.check_any(commands.is_owner(), commands.has_permissions(administrator = True))
@@ -73,20 +91,22 @@ async def update(context):
             if file_name.endswith(".py"):
                 client.reload_extension(f"cogs.{file_name[:-3]}")
     except Exception as error_message:
-        await context.send(embed = create_embed(f"Error: Unable to update bot", discord_color.red(), {
+        await context.send(embed = create_embed(f"ERROR: Unable to update bot", {
             "Error Message": str(error_message)
+        }, {
+            "color": discord_color.red(),
+            "member":  context.author,
         }))
     else:
-        await context.send(embed = create_embed(f"Success: Bot was updated", discord_color.green()))
+        await context.send(embed = create_embed(f"SUCCESS: Bot was updated", {}, {
+            "color": discord_color.green(),
+            "member":  context.author,
+        }))
 
-def main():
-    client.remove_command("help")
+client.remove_command("help")
 
-    for file_name in os.listdir("./cogs"):
-            if file_name.endswith(".py"):
-                client.load_extension(f"cogs.{file_name[:-3]}")
+for file_name in os.listdir("./cogs"):
+    if file_name.endswith(".py"):
+        client.load_extension(f"cogs.{file_name[:-3]}")
 
-    client.run(TOKEN)
-
-if __name__ == "__main__":
-    main()
+client.run(TOKEN)
