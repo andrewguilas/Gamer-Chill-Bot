b512f3e57a50829181621e11f90c103281cd8abb
Vex87 <AndrewKyleGuilas@gmail.com>
2021-03-31 21:35:41 -0400

    added personal roblox notifier

---
+++ cogs/personal.py
@@ -0,0 +1,73 @@
+STATUS_URL = "http://api.roblox.com/users/USER_ID/onlinestatus/"
+USERNAME_URL = "http://api.roblox.com/users/USER_ID"
+USER_ID = 353933582
+UPDATE_DELAY = 5
+USER_TO_NOTIFY = 496001579292295169
+
+OFFLINE_EMOJI = "⚫"
+ONLINE_EMOJI = "🔵"
+PLAYING_EMOJI = "🟢"
+
+import discord
+from discord import Color as discord_color
+from discord.ext import commands, tasks
+
+import math
+import pytz
+import os
+import asyncio
+
+import requests
+import time
+
+from datetime import datetime
+
+def create_embed(title, fields: {} = {}, info: {} = {}):
+    embed = discord.Embed(
+        title = title,
+        colour = info.get("color") or discord_color.blue(),
+        timestamp = datetime.now(tz = pytz.timezone("US/Eastern"))
+    )
+
+    for name, value in fields.items():
+        embed.add_field(
+            name = name,
+            value = value,
+            inline = True
+        )
+
+    if info.get("member"):
+        embed.set_author(name = info["member"], icon_url = info["member"].avatar_url)
+
+    return embed
+
+class personal(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.current_status = None
+        self.roblox_user_status.start()
+
+    def cog_unload(self):
+        self.roblox_user_status.cancel()
+
+    def cog_load(self):
+        self.roblox_user_status.start()
+
+    @tasks.loop(seconds = UPDATE_DELAY)
+    async def roblox_user_status(self):
+        user = await self.client.fetch_user(USER_TO_NOTIFY)
+        user_data = requests.get(USERNAME_URL.replace("USER_ID", str(USER_ID))).json()
+        status_data = requests.get(STATUS_URL.replace("USER_ID", str(USER_ID))).json()
+
+        status = status_data["LastLocation"].lower()
+        if self.current_status != status:
+            self.current_status = status
+            await user.send("{status_emoji} {username} is **{status}** ({current_time})".format(
+                status_emoji = status == "offline" and OFFLINE_EMOJI or status == "online" and ONLINE_EMOJI or status == "playing" and PLAYING_EMOJI,
+                username = user_data["Username"],
+                status = self.current_status,
+                current_time = datetime.now(tz = pytz.timezone("US/Eastern")).strftime("%m/%d/%y - %I:%M %p")
+            ))
+
+def setup(client):
+    client.add_cog(personal(client))
