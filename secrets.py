5c3e4f69e55dde422abe39f32a21ddd9dbc89703
Vex87 <AndrewKyleGuilas@gmail.com>
2021-04-18 19:59:00 -0400

    removed secrets.py from .gitignore

---
+++ secrets.py
@@ -0,0 +1,11 @@
+TOKEN = "ODEzMjU4Njg3MjI5NDYwNDkw.YDMsLA.Tc2QvlTr_-SBZnoJ88W8SLSZUUE"
+TEST_TOKEN = "ODI3MjIzMDExNzM5ODI4MjM0.YGX5dw.x0ZjP8RgiV1JujgmAdlIkimsGKQ"
+MONGO_TOKEN = "mongodb+srv://admin:QZnOT86qe3TQ@cluster0.meksl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+CLIENT_ID = "813258687229460490"
+REDDIT_INFO = {
+    "client_id": "cTdcYAFeJKqwKg",
+    "client_secret": "QO0zyzU_pB8voB0nQUvfXIRgqmL02g", 
+    "password": "6&OB7s4PcCKz&08o",
+    "user_agent": "gamer-chill-bot",
+    "username": "Hast1e"
+}
