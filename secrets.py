813a74e9c354e478621ff66d04004ca71c26a035
Vex87 <AndrewKyleGuilas@gmail.com>
2021-04-20 19:48:58 -0400

    switched env var method

--- secrets.py
+++ secrets.py
@@ -1,13 +1,13 @@
 import os
 
-TOKEN = os.environ.get("GCB_TOKEN")
-TEST_TOKEN = os.environ.get("GCB_TEST_TOKEN")
-MONGO_TOKEN = os.environ.get("GCB_DB_TOKEN")
-CLIENT_ID = os.environ.get("GCB_CLIENT_ID")
+TOKEN = os.getenv("GCB_TOKEN")
+TEST_TOKEN = os.getenv("GCB_TEST_TOKEN")
+MONGO_TOKEN = os.getenv("GCB_DB_TOKEN")
+CLIENT_ID = os.getenv("GCB_CLIENT_ID")
 REDDIT_INFO = {
-    "client_id": os.environ.get("GCB_REDDIT_CLIENT_ID"),
-    "client_secret": os.environ.get("GCB_REDDIT_CLIENT_SECRET"),
-    "password": os.environ.get("GCB_REDDIT_PASSWORD"),
-    "user_agent": os.environ.get("GCB_REDDIT_USER_AGENT"),
-    "username": os.environ.get("GCB_REDDIT_USERNAME"),
+    "client_id": os.getenv("GCB_REDDIT_CLIENT_ID"),
+    "client_secret": os.getenv("GCB_REDDIT_CLIENT_SECRET"),
+    "password": os.getenv("GCB_REDDIT_PASSWORD"),
+    "user_agent": os.getenv("GCB_REDDIT_USER_AGENT"),
+    "username": os.getenv("GCB_REDDIT_USERNAME"),
 }
