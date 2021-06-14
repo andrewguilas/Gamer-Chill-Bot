4b2157c86b3dadfee2f6758fb4f1f0c78f063fce
Vex87 <AndrewKyleGuilas@gmail.com>
2021-06-13 23:29:54 -0400

    stopped testing

--- constants.py
+++ constants.py
@@ -458,7 +458,7 @@ NEXT_EMOJI = "▶️"
 BACK_EMOJI = "◀️"
 CHANGE_EMOJI = "\N{gear}"
 
-IS_TESTING = True
+IS_TESTING = False
 CURRENT_VERSION = "9.3.0"
 LIVE_DATASTORE = "datastore5"
 TESTING_DATASTORE = "datastore2"
